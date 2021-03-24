18c1eb6dd3c7929554fccbd24f1ae28cc1383369
Histefanhere <stefan.zdra@gmail.com>
2021-03-24 18:07:24 +1300

    Actually decent documentation and commenting

--- network.py
+++ network.py
@@ -8,10 +8,31 @@ class LinkExists(Exception):
     pass
 
 def get_template(name):
+    """Return the file path & name of an html template file."""
     with open(f"templates/{name}.html", "r") as f:
         return f.read()
 
 class Page:
+    """A page in the webmaze.
+    
+    Each page of the webmaze can be thought of as a node in a network graph,
+    with many links to other pages in the network.
+
+    Args:
+        index (int): The index of the page and its (internal) identifier.
+        code (str): A random string used as the page's filename.
+        image (str): The name of the image used for the page.
+        maze (Maze): A reference to the master maze object.
+    
+    Attributes:
+        links (list[Page]): A list of all the page's links.
+        index (int): The index of the page and its (internal) identifier.
+        code (str): A random string used as the page's filename.
+        image (str): The name of the image used for the page.
+        maze (Maze): A reference to the master maze object.
+        filename (str): The page's future filename, generated from it's code.
+    """
+    
     def __init__(self, index, code, image, maze):
         self.links = []
         self.index = index
@@ -21,6 +42,7 @@ class Page:
         # Store a reference of the master maze object
         self.maze = maze
 
+        # Figure out the filename of the page based on our code (set to `0.html` if this page is the first)
         if self.index == 0:
             self.filename = "0.html"
         else:
@@ -31,7 +53,7 @@ class Page:
         print(f"Links: {self.links}")
     
     def create(self):
-        # Generate an html page and plop it into the maze
+        """Generate an HTML file for the page."""
         if self.index == 0:
             template = get_template('start_page')
         elif self.index == self.maze.get_end().index:
@@ -58,36 +80,67 @@ class Page:
             file.write(template)
 
 class Maze:
+    """Object representing the whole maze. 
+
+    This handles everything to do with managing the network of pages (nodes).
+
+    Attributes:
+        size (int): Number of pages in the maze.
+        pages (list[Page]): A list of all the pages in the network.
+        image_names (list[str]): List of all available image names.
+    """
+
     def __init__(self):
         self.pages = []
         self.size = 0
 
-        # Only for reference, so a code is never repeated
-        self.codes = []
-        self.images = []
+        # Only for reference, so a code or image is never repeated
+        self._codes = []
+        self._images = []
 
         with open('things.json', 'rb') as file:
             self.image_names = json.load(file)
 
+            random.shuffle(self.image_names)
+
     def generate(self, n):
-        # Generate n different pages
+        """Generate n different pages.
+
+        Here we handle things like making sure each page has a unique
+        code and image assigned to them.
+        """
+
         for _ in range(n):
-            self.add_page()
+            index = self.size
+            code = self._generate_code()
+            image = self._generate_image()
 
-    def add_page(self):
-        index = self.size
-        code = self.generate_code()
-        image = self.generate_image()
-
-        page = Page(index, code, image, self)
-        self.pages.append(page)
-        self.size += 1
+            page = Page(index, code, image, self)
+            self.pages.append(page)
+            self.size += 1
     
+    def get_page(self, index):
+        """Return the page at index."""
+        return self.pages[index]
+
     def get_end(self):
-        # Return the last node, i.e. the destination
-        return self.pages[-1]
+        """Return the last node, i.e. the destination."""
+        return self.get_page(-1)
 
     def link(self, ai, bi):
+        """Link two differnet pages together.
+
+        Each page gets a copy of the other in their links attributes.
+
+        Args:
+            ai (int): Index of the first node to link.
+            bi (int): Index of the second node to link.
+
+        Raises:
+            LinkExists: A link already exists between pages a and b.
+        """
+        if ai == bi:
+            raise ValueError("Cannot link a page to itself")
         a = self.pages[ai]
         b = self.pages[bi]
         if b not in a.links:
@@ -95,29 +148,49 @@ class Maze:
             b.links.append(a)
         else:
             raise LinkExists()
-
-    def get_page(self, index):
-        return self.pages[index]
     
-    def generate_image(self):
-        while True:
-            image = random.choice(self.image_names)
-            if image not in self.images:
-                self.images.append(image)
-                return image
-
-    def generate_code(self):
-        while True:
-            code = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(10))
-            if code not in self.codes:
-                self.codes.append(code)
-                return code
-
     def create(self):
-        # Delete all files in maze
+        """Create the HTML maze.
+
+        This method will delete all existing HTML files from `maze` and create a new maze
+        based on the maze's pages and their links.
+        """
+        # First, delete any old HTML files from the maze
         files = glob.glob('maze/*.html')
         for f in files:
             os.remove(f)
 
+        # Now tell each page to create their individual pages
         for page in self.pages:
             page.create()
+    
+    def _generate_image(self):
+        """Return a random image name that hasn't been chosen till now.
+
+        The image name will also be stored so that it never gets repeated. This
+        method should only be called when creating a new page.
+        
+        Returns:
+            A unique image name.
+        """
+        while True:
+            image = random.choice(self.image_names)
+            if image not in self._images:
+                self._images.append(image)
+                return image
+
+    def _generate_code(self):
+        """Return a randomized code that hasn't been chosen till now.
+
+        The code is a random string consisting of letters and digits that are URL
+        compatible. It will also be stored so that it never gets repeated. This
+        method should only be called when creating a new page.
+
+        Returns:
+            A unique code.
+        """
+        while True:
+            code = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(10))
+            if code not in self._codes:
+                self._codes.append(code)
+                return code
--- webmaze.py
+++ webmaze.py
@@ -3,21 +3,22 @@ import random
 
 # http://people.uncw.edu/tagliarinig/Courses/380/F2017%20papers%20and%20presentations/The%20Amazing%20Maze--Lawrence,%20Neill,%20and%20Nobles/The%20Amazing%20Maze(1).pdf
 
-maze = network.Maze()
-
 n = 20
 chance = 0.4
 
+# Firstly generate an n-sized maze of pages
+maze = network.Maze()
 maze.generate(n)
 
-# make sure the start and end are connected
+# This is an algorithm I came up with for generating a simple maze, purely for testing purposes.
+# The first loop generates a path of links that's guaranteed to link the starting page to the ending one.
+# The second loop then randomly creates links between all other pairs of pages, according to `chance`.
 prev = 0
 cur = 0
 while cur < n - 1:
     cur = min(n - 1, cur + random.randint(1, 3))
     maze.link(prev, cur)
     prev = cur
-
 
 for i in range(0, n - 2):
     for j in range(i + 1, n - 1):
@@ -27,6 +28,5 @@ for i in range(0, n - 2):
             except network.LinkExists:
                 pass
 
-# maze.link(0, 1)
-
+# Create the HTML pages
 maze.create()
